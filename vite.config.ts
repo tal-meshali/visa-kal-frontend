d57b33db91c0bd54648971c1d05f099589350498
Tal Meshali <meshalital@gmail.com>
2026-01-09 17:15:55 +0200

    ??

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@
 // @ts-ignore - vitest/config extends vite config with test property
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import type { PluginOption } from 'vite'
+import type { UserConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() as PluginOption],
+  plugins: [react()] as UserConfig["plugins"],
   test: {
     globals: true,
     environment: 'jsdom',
