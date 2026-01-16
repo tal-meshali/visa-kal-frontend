86928d681daf3060ddab04c9829911c8eebb1f27
Tal Meshali <meshalital@gmail.com>
2026-01-16 16:38:26 +0200

    Better fields

--- src/utils/fieldRenderer.tsx
+++ src/utils/fieldRenderer.tsx
@@ -1,153 +1,98 @@
 import React from "react";
-import DateFieldComponent from "../components/fields/DateField";
-import DocumentFieldComponent from "../components/fields/DocumentField";
-import NumberFieldComponent from "../components/fields/NumberField";
-import PhotoFieldComponent from "../components/fields/PhotoField";
-import SelectFieldComponent from "../components/fields/SelectField";
-import StringFieldComponent from "../components/fields/StringField";
-import {
-  type CommonFieldProps,
-  type DateField,
-  type DocumentField,
-  type NumberField,
-  type PhotoField,
-  type SelectField,
-  type StringField,
-  type TypedFormField,
-} from "../types/formTypes";
+import DateField from "../components/fields/DateField";
+import DocumentField from "../components/fields/DocumentField";
+import NumberField from "../components/fields/NumberField";
+import PhotoField from "../components/fields/PhotoField";
+import SelectField from "../components/fields/SelectField";
+import StringField from "../components/fields/StringField";
 
-/**
- * Type guard to check if field is a StringField
- */
-function isStringField(field: TypedFormField): field is StringField {
-  return field.field_type === "string";
+interface FormField {
+  name: string;
+  label: { en: string; he: string };
+  field_type: "string" | "number" | "date" | "select" | "document" | "photo";
+  required?: boolean;
+  placeholder?: { en: string; he: string };
+  [key: string]: any;
+  default_value?: string;
 }
 
-/**
- * Type guard to check if field is a NumberField
- */
-function isNumberField(field: TypedFormField): field is NumberField {
-  return field.field_type === "number";
+interface CommonFieldProps {
+  name: string;
+  label: { en: string; he: string };
+  value: any;
+  onChange: (value: any) => void;
+  error?: string;
+  required: boolean;
+  placeholder?: { en: string; he: string };
+  language: "en" | "he";
+  fieldId?: string;
+  beneficiaryId?: string;
+  getToken?: () => Promise<string | null>;
+  onUploadStateChange?: (uploadId: string, isUploading: boolean) => void;
+  activeUploads?: Set<string>;
 }
 
-/**
- * Type guard to check if field is a DateField
- */
-function isDateField(field: TypedFormField): field is DateField {
-  return field.field_type === "date";
-}
+type FieldRenderer = (
+  field: FormField,
+  props: CommonFieldProps
+) => React.JSX.Element | null;
 
-/**
- * Type guard to check if field is a SelectField
- */
-function isSelectField(field: TypedFormField): field is SelectField {
-  return field.field_type === "select";
-}
-
-/**
- * Type guard to check if field is a DocumentField
- */
-function isDocumentField(field: TypedFormField): field is DocumentField {
-  return field.field_type === "document";
-}
-
-/**
- * Type guard to check if field is a PhotoField
- */
-function isPhotoField(field: TypedFormField): field is PhotoField {
-  return field.field_type === "photo";
-}
+const fieldRenderers: Record<string, FieldRenderer> = {
+  string: (field, props) => (
+    <StringField
+      {...props}
+      minLength={field.min_length}
+      maxLength={field.max_length}
+    />
+  ),
+  number: (field, props) => (
+    <NumberField
+      {...props}
+      min={field.min_value}
+      max={field.max_value}
+      step={field.step}
+    />
+  ),
+  date: (field, props) => (
+    <DateField {...props} minDate={field.min_date} maxDate={field.max_date} />
+  ),
+  select: (field, props) => (
+    <SelectField
+      {...props}
+      options={field.options || []}
+      multiple={field.multiple}
+    />
+  ),
+  document: (field, props) => (
+    <DocumentField
+      {...props}
+      fieldId={props.fieldId || props.name}
+      beneficiaryId={props.beneficiaryId || "0"}
+      acceptedFormats={field.accepted_formats}
+      maxSizeMB={field.max_size_mb}
+      onUploadStateChange={props.onUploadStateChange}
+    />
+  ),
+  photo: (field, props) => (
+    <PhotoField
+      {...props}
+      fieldId={props.fieldId || props.name}
+      beneficiaryId={props.beneficiaryId || "0"}
+      acceptedFormats={field.accepted_formats}
+      maxSizeMB={field.max_size_mb}
+      onUploadStateChange={props.onUploadStateChange}
+      activeUploads={props.activeUploads}
+    />
+  ),
+};
 
 export const renderFormField = (
-  field: TypedFormField,
+  field: FormField,
   commonProps: CommonFieldProps
-): React.JSX.Element | null => {
-  const key = `${commonProps.beneficiaryId}__${field.name}`;
-  if (isStringField(field)) {
-    return (
-      <StringFieldComponent
-        {...commonProps}
-        key={key}
-        value={commonProps.value as string}
-        onChange={(value) => commonProps.onChange(value)}
-        minLength={field.min_length}
-        maxLength={field.max_length}
-      />
-    );
+) => {
+  const renderer = fieldRenderers[field.field_type];
+  if (!renderer) {
+    return null;
   }
-
-  if (isNumberField(field)) {
-    return (
-      <NumberFieldComponent
-        {...commonProps}
-        key={key}
-        value={commonProps.value as number | string}
-        onChange={(value) => commonProps.onChange(value)}
-        min={field.min_value}
-        max={field.max_value}
-        step={field.step}
-      />
-    );
-  }
-
-  if (isDateField(field)) {
-    return (
-      <DateFieldComponent
-        {...commonProps}
-        key={key}
-        value={commonProps.value as string}
-        onChange={(value) => commonProps.onChange(value)}
-        minDate={field.min_date}
-        maxDate={field.max_date}
-      />
-    );
-  }
-
-  if (isSelectField(field)) {
-    return (
-      <SelectFieldComponent
-        {...commonProps}
-        key={key}
-        value={commonProps.value as string}
-        onChange={(value) => commonProps.onChange(value)}
-        options={field.options || []}
-        multiple={field.multiple}
-      />
-    );
-  }
-
-  if (isDocumentField(field)) {
-    return (
-      <DocumentFieldComponent
-        {...commonProps}
-        key={key}
-        value={commonProps.value as string | null}
-        onChange={(value) => commonProps.onChange(value ?? "")}
-        fieldId={commonProps.fieldId || commonProps.name}
-        beneficiaryId={commonProps.beneficiaryId || "0"}
-        acceptedFormats={field.accepted_formats}
-        maxSizeMB={field.max_size_mb}
-        onUploadStateChange={commonProps.onUploadStateChange}
-      />
-    );
-  }
-
-  if (isPhotoField(field)) {
-    return (
-      <PhotoFieldComponent
-        {...commonProps}
-        key={key}
-        value={commonProps.value as string | null}
-        onChange={(value) => commonProps.onChange(value ?? "")}
-        fieldId={commonProps.fieldId || commonProps.name}
-        beneficiaryId={commonProps.beneficiaryId || "0"}
-        acceptedFormats={field.accepted_formats}
-        maxSizeMB={field.max_size_mb}
-        onUploadStateChange={commonProps.onUploadStateChange}
-        requestId={commonProps.requestId}
-      />
-    );
-  }
-
-  return null;
+  return renderer(field, commonProps);
 };
